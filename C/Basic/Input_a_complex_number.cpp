3109448d51dea64dd7bd7463b8fe94762f66cf45
Giang <gnguyen899@gmail.com>
2023-08-04 14:26:40 +0700

    Commit 8

---
+++ C/Basic/Input_a_complex_number.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace
+
+class Complex_number
+{
+    public:
+    int real;
+    int image;
+    void input_function();
+    void output_funciton();
+
+};
+
+void Complex_number::input_function()
+{
+    cout << "Input value of real part: " << endl;
+    cin >> real;
+
+    cout << "Input value of image part: " << endl;
+    cin >> image;
+
+}
+
+void Complex_number::output_funciton()
+{
+    cout << "The complex number is: " << real << " + " << image << "i" << endl;
+} 
+
+
+int main()
+{
+    Complex_number complex_number;
+    complex_number.input_function();
+    cout << "-----------------------------" << endl;
+    complex_number.output_funciton();
+}
