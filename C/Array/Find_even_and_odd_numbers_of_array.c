e2f7d3494eff96a0568a52b2b56d5eb0184ae8f2
Nguyen Truong Giang <gnguyen899@gmail.com>
2023-08-30 10:06:49 +0700

    Find even and odd numbers of array

---
+++ C/Array/Find_even_and_odd_numbers_of_array.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main()
+{
+    int i, n;
+	int min, max;
+	printf("Input number of elements of array: ");
+	scanf("%d", &n);
+	int array[n];
+	printf("Input elements of array:\n");
+	for(i = 0; i < n; i++)
+	{
+		printf("Array[%d] = ", i);
+		scanf("%d", &array[i]);
+	}
+	printf("--------------------------------\n");
+	
+	printf("The even numbers of array:\n");
+	for(i = 0; i < n; i++)
+	{		
+		if(array[i] % 2 == 0)
+		{
+			printf("Array[%d] = %d\n", i, array[i]);
+		}
+	}
+	
+	printf("The odd numbers of array:\n");
+	for(i = 0; i < n; i++)
+	{	
+		if(array[i] % 2 != 0)
+		{
+			printf("Array[%d] = %d\n", i, array[i]);
+		}
+	}
+}
