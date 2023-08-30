0a0910a756aa1c542e8be050f74d0bb17ee6e3ab
Nguyen Truong Giang <gnguyen899@gmail.com>
2023-08-30 07:53:33 +0700

    Input and Display Elements of Array

---
+++ C/Array/Input_and_Display_Elements_of_Array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int i, n;
+	printf("Input number of elements of array: ");
+	scanf("%d", &n);
+	
+	int array[n];
+	
+	for(i = 0; i < n; i++)
+	{
+		printf("Array[%d] = ", i);
+		scanf("%d", &array[i]);
+	}
+	printf("--------------------------------\n");
+	printf("The elements of array are:\n");
+	for(i = 0; i < n; i++)
+	{
+		printf("Array[%d] = %d\n", i, array[i]);
+	}
+}
