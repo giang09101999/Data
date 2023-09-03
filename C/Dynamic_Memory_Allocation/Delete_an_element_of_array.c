4347c02c259d0874a651c39757a436e7173e5a42
Nguyen Truong Giang <gnguyen899@gmail.com>
2023-09-03 20:55:47 +0700

    Delete an element of array

---
+++ C/Dynamic_Memory_Allocation/Delete_an_element_of_array.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main()
+{
+	int i, n, *p, position;
+	printf("Input number of element of array: ");
+	scanf("%d", &n);
+	int a = n;
+	p = (int*)malloc(n*sizeof(int));
+	
+	for(i = 0; i < n; i++)
+	{
+		printf("Array[%d] = ", i);
+		scanf("%d", (p + i));
+	}
+	
+	p = realloc(p, (n - 1)*sizeof(int));
+	printf("----------------------------------\n");
+	printf("Select position you want to delete: ");
+	scanf("%d", &position);
+	
+	for(i = position; i < n; i++)
+	{
+		*(p + i) = *(p + (i + 1));
+	}
+
+	printf("----------------------------------\n");
+	printf("The array after deleting:\n");
+	for(i = 0; i < n - 1; i++)
+	{
+		printf("Array[%d] = %d\n", i, *(p + i));
+	}	
+	
+}
