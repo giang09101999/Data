9ca5c99a9bc30bd5b8d7ca6beda5aa754a8e8129
Nguyen Truong Giang <gnguyen899@gmail.com>
2023-09-03 14:23:13 +0700

    Decreasing array

---
+++ C/Dynamic_Memory_Allocation/Decreasing_array.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+	int i, n, j, *p;
+	printf("Input number of elements of array: ");
+	scanf("%d", &n);
+
+	p = (int*)malloc(n*sizeof(int));
+	
+	for(i = 0; i < n; i++)
+	{
+		printf("Array[%d] = ", i);
+		scanf("%d", (p + i));
+	}
+	
+	for(i = 0; i < n - 1; i++)
+	{
+		for(j = i + 1; j < n; j++)
+		{
+			if(*(p + i) < *(p + j))
+			{
+				int tempt = *(p + i);
+				*(p + i) = *(p + j);
+				*(p + j) = tempt;
+			}
+		}
+	}
+	
+	printf("-----------------------------\n");
+	printf("The decreasing array:\n");
+	for(i = 0; i < n; i++)
+	{
+		printf("Array[%d] = %d\n", i, *(p + i));
+	}
+}
