dd73effb3c2bde06186ab64d048ecd6fcad35ae3
Nguyen Truong Giang <gnguyen899@gmail.com>
2023-09-07 09:47:38 +0700

    Append an element into array

---
+++ C++/Vector/Append_an_element_into_array.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int i, n, value, position, append_value;
+    vector<int> array;
+    
+    cout << "Input number of elements of array: ";
+    cin >> n;
+    
+    for(i = 0; i < n; i++)
+    {
+        cout << "Array[" << i << "] = ";
+        cin >> value;
+        array.push_back(value);
+    }
+    
+    cout << "Select position you want to append element: ";
+    cin >> position;
+    cout << "Input value you want to append: ";
+    cin >> append_value;
+    
+    array.insert(array.begin() + position, append_value);
+    n++;
+    cout << "------------------------------------------" << endl;
+    cout << "The array: " << endl;
+    
+    for(i = 0; i < n; i++)
+    {
+        cout << "Array[" << i << "] = " << array[i] << endl;
+    }
+    return 0;
+}
