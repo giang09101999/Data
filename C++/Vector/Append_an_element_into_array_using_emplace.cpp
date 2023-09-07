d5995c7684331828c116d6f02757277a311e5182
Nguyen Truong Giang <gnguyen899@gmail.com>
2023-09-07 10:20:29 +0700

    Append an element into array using emplace

---
+++ C++/Vector/Append_an_element_into_array_using_emplace.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int i, n, value, position, append_value;
+    vector<int> array;
+    
+    cout << "Input number of elements of array: ";
+    cin >> n;
+    
+    for(i = 0; i < n; i++)
+    {
+        cout << "Array[" << i << "] = ";
+        cin >> value;
+        array.push_back(value);
+    }
+    
+    cout << "Select position you want to append: ";
+    cin >> position;
+    cout << "Input value you want to append:";
+    cin >> append_value;
+    array.emplace(array.begin() + position, append_value);
+    n++;
+    cout << "---------------------------------------" << endl;
+    cout << "The array: " << endl;
+    for(i = 0; i < n; i++)
+    {
+        cout << "Array[" << i << "] = " << array[i] << endl;
+    }   
+}
