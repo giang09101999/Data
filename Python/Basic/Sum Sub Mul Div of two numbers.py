5c0350c228a6163a64c437a78ed5e93f6d1532d6
Nguyen Truong Giang <gnguyen899@gmail.com>
2023-09-18 09:20:09 +0700

    Sum Substraction Multiple Division of two numbers

---
+++ Python/Basic/Sum Sub Mul Div of two numbers.py
@@ -0,0 +1,11 @@
+x, y = [int(x) for x in input("Enter value: ").split()]
+
+Sum = x + y
+Sub = x - y
+Mul = x*y
+Div = x/y
+
+print("The sum:", Sum)
+print("The substraction:", Sub)
+print("The multiple:", Mul)
+print("The division:", Div)
