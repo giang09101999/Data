8973bcad6d22a805f39032640852426ee3638806
Nguyen Truong Giang <gnguyen899@gmail.com>
2023-09-18 21:00:39 +0700

    Check number of times characters appears in string

---
+++ Python/Data type/String/Check number of times characters appears in string.py
@@ -0,0 +1,7 @@
+string = input("Input string: ")
+
+checked_characters = input("Input the desired characters: ")
+
+count = string.count(checked_characters)
+
+print("The number of times " + checked_characters + " appears in string is:", count)
