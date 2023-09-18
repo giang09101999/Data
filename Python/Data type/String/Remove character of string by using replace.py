aabf7bee171d19ae241d02dc36f1e4ff112c84f2
Nguyen Truong Giang <gnguyen899@gmail.com>
2023-09-18 10:07:33 +0700

    Remove character of string by using replace

---
+++ Python/Data type/String/Remove character of string by using replace.py
@@ -0,0 +1,7 @@
+string_name = input("Input string: ")
+
+character = input("Input a character of string you want to delete: ")
+
+new_string = string_name.replace(character, "")
+
+print(new_string)
