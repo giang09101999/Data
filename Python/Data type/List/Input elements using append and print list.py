adefdbcdab51327b79fed061c24c5e73f34846e0
Nguyen Truong Giang <gnguyen899@gmail.com>
2023-09-19 21:05:33 +0700

    Input elements using append and print list

---
+++ Python/Data type/List/Input elements using append and print list.py
@@ -0,0 +1,10 @@
+List = []
+
+size = int(input("Input number of elements: "))
+
+for i in range(size):
+    element = int(input("List[" + str(i) + "] = "))
+    List.append(element)
+
+print(List)
+
