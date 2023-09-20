dc4cc660c790ad8d313ea44d51c9c3cf3bc9fc0b
giang09101999 <Gnguyen899@gmail.com>
2023-09-20 21:09:16 +0700

    Delete desired duplicate elements of list

---
+++ Python/Data type/List/Delete desired duplicate elements of list.py
@@ -0,0 +1,25 @@
+List =[]
+count = 0
+size = int(input("Input number of elements of list: "))
+
+for i in range(size):
+    elements = int(input("List[" + str(i) + "] = " ))
+    List.append(elements)
+    
+desired_elements = int(input("Input desired number you want to delete: "))
+
+for i in List:
+    if i == desired_elements:
+        count = count + 1
+        break
+    else:
+        count = 0
+
+print(count)
+if count == 0:
+    print("List: ", List)
+    print("This number doesn't exist in list")
+else:
+    while desired_elements in List:
+        List.remove(desired_elements)
+    print("List: ", List)
