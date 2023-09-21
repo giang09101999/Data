9473c0ec5e0798b38a41af73de518d6aa98546ef
giang09101999 <Gnguyen899@gmail.com>
2023-09-21 12:01:11 +0700

    Check number of specific element appears in list

--- Python/Data type/List/Check number of specific element appears in list.py
+++ Python/Data type/List/Check number of specific element appears in list.py
@@ -14,7 +14,7 @@ for i in List:
         break
 
 if count == 0:
-    print("There is no element of" + str(specific_number) + " appears in list")
+    print("There is no element of " + str(specific_number) + " appears in list")
 else:
     Count = List.count(specific_number)
     print("Element " + str(specific_number) + " appears " + str(Count) + " times in list")    
