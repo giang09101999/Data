581a28375a28440521a184d20804492334501c68
giang09101999 <Gnguyen899@gmail.com>
2023-09-21 20:46:05 +0700

    Find max

---
+++ Python/Data type/Tuple/Find max.py
@@ -0,0 +1,9 @@
+String_Tuple = ("a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "a", "aa", "a")
+print("String Tuple: ", String_Tuple)
+Max_String_Tuple = max(String_Tuple)
+print("The maximum of String Tuple:", Max_String_Tuple)
+
+Int_Tuple = (1, 2, 4, 3, 9, 0, 10, 6, 7, 8)
+print("Int Tuple: ", Int_Tuple)
+Max_Int_Tuple = max(Int_Tuple)
+print("The maximum of Int Tuple:", Max_Int_Tuple)
