d6568f0e2781676adee1cb14b959f73005c19373
giang09101999 <Gnguyen899@gmail.com>
2023-09-21 11:41:49 +0700

    Sum of elements of list

---
+++ Python/Data type/List/Sum of elements of list.py
@@ -0,0 +1,11 @@
+List = []
+
+size = int(input("Input number of elements of list: "))
+
+for i in range(size):
+    element = int(input("List[" + str(i) + "] = "))
+    List.append(element)
+
+Sum = sum(List)
+
+print("The sum is:", Sum)
