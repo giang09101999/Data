2dc752619fa219c7b458714e7ff06002088e143a
giang09101999 <Gnguyen899@gmail.com>
2023-09-21 20:27:08 +0700

    Access a specific of tuple

---
+++ Python/Data type/Tuple/Access a specific of tuple.py
@@ -0,0 +1,7 @@
+Tuple = (1, 2, 3, 4, 5, 6, "John", 7, 8)
+
+print("Tuple: ", Tuple)
+
+position = int(input("Choose a specific position of tuple you want to access: "))
+print("The value at " + str(position) + " is: " + str(Tuple[position]))
+
