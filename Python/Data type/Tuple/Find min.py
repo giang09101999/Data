6b9e296efd024896eeb60f240466560f24fdd277
giang09101999 <Gnguyen899@gmail.com>
2023-09-21 20:48:23 +0700

    Find min

---
+++ Python/Data type/Tuple/Find min.py
@@ -0,0 +1,9 @@
+String_Tuple = ("a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "a", "aa", "a")
+print("String Tuple: ", String_Tuple)
+Min_String_Tuple = min(String_Tuple)
+print("The minimum of String Tuple:", Min_String_Tuple)
+
+Int_Tuple = (1, 2, 4, 3, 9, 0, 10, 6, 7, 8)
+print("Int Tuple: ", Int_Tuple)
+Min_Int_Tuple = min(Int_Tuple)
+print("The minimum of Int Tuple:", Min_Int_Tuple)
