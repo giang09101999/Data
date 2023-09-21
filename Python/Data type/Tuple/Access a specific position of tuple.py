5bf57d33cbe21c13b585652afa50e5e42b25de08
giang09101999 <Gnguyen899@gmail.com>
2023-09-21 20:35:28 +0700

    Access a specific position of tuple

--- Python/Data type/Tuple/Access a specific position of tuple.py
+++ Python/Data type/Tuple/Access a specific position of tuple.py
@@ -2,6 +2,11 @@ Tuple = (1, 2, 3, 4, 5, 6, "John", 7, 8)
 
 print("Tuple: ", Tuple)
 
+
 position = int(input("Choose a specific position of tuple you want to access: "))
-print("The value at " + str(position) + " is: " + str(Tuple[position]))
+
+if (position < len(Tuple))&(position >= 0):
+    print("The value at " + str(position) + " is: " + str(Tuple[position]))
+else:
+    print("Out of range")
 
