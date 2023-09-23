b615891120e9b59c9eb8d81aed4187ff84a31969
giang09101999 <Gnguyen899@gmail.com>
2023-09-23 08:46:37 +0700

    Find even numbers

---
+++ Python/Data type/List/Exercises/Find even numbers.py
@@ -0,0 +1,12 @@
+List = []
+
+size = int(input("Input number of elements of list: "))
+
+for i in range(size):
+    element = int(input("List[" + str(i) + "] = "))
+    List.append(element)
+
+print("The even numbers is:")
+for num in List:
+    if (num % 2) == 0:
+        print(num)
