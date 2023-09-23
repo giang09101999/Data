849794d7e7eba3e419dbe15805df9f9ba8efbf62
giang09101999 <Gnguyen899@gmail.com>
2023-09-23 08:28:14 +0700

    Swap two elements in a list

---
+++ Python/Data type/List/Exercises/Swap two elements in a list.py
@@ -0,0 +1,20 @@
+List = []
+count = 0
+size = int(input("Input number of elements of list: "))
+
+for i in range(size):
+    element = int(input("List[" + str(i) + "] = "))
+    List.append(element)
+
+
+
+print("Input elements you want to swap in list: ")
+
+Pos_1 = int(input("Position 1: "))
+Pos_2 = int(input("Position 2: "))
+
+print("Original List:", List)
+tempt = List[Pos_1]
+List[Pos_1] = List[Pos_2]
+List[Pos_2] = tempt
+print("Swapped List:", List)
