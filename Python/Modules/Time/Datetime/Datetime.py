9084639a1c5f9c4e8d63070e1d3af8c0fc19d905
giang09101999 <Gnguyen899@gmail.com>
2023-09-25 14:59:52 +0700

    Create an array

--- Python/Modules/Time/Datetime/Datetime.py
+++ Python/Modules/Time/Datetime/Datetime.py
@@ -1,6 +1,9 @@
 from datetime import datetime
+from datetime import date
 
 time = datetime.today()
+weekday = date.weekday()
+
 
 print("Year:", time.year)
 print("Month:", time.month)
@@ -9,3 +12,5 @@ print("---------------")
 print("Hour:", time.hour)
 print("Minute:", time.minute)
 print("Second:", time.second)
+
+print("Weekday", weekday.weekday)
