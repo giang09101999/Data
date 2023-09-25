5dbb175a318eb517784c102f1a0e11cba118aea7
giang09101999 <Gnguyen899@gmail.com>
2023-09-25 10:51:59 +0700

    Datetime

---
+++ Python/Modules/Time/Datetime/Datetime.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+time = datetime.today()
+
+print("Year:", time.year)
+print("Month:", time.month)
+print("Day:", time.day)
+print("---------------")
+print("Hour:", time.hour)
+print("Minute:", time.minute)
+print("Second:", time.second)
