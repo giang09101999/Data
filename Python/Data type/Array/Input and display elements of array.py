afa2aae0734e1701d6cc2770122ab7863d27b4af
giang09101999 <Gnguyen899@gmail.com>
2023-09-25 15:56:30 +0700

    Input and display elements of array

---
+++ Python/Data type/Array/Input and display elements of array.py
@@ -0,0 +1,11 @@
+import array as arr
+
+array = arr.array('i', [])
+
+size = int(input("Input number of elements of array: "))
+
+for i in range(size):
+    element = int(input("Array[" + str(i) + "] = "))
+    array.append(element)
+    
+print("The array:", *array)
