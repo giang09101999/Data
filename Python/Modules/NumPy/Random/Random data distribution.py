5ba6bd4f4e24c2988fa62ecfc96f39e4aad603c0
giang09101999 <Gnguyen899@gmail.com>
2023-09-28 10:17:33 +0700

    Random data distribution

--- Python/Modules/NumPy/Random/Random data distribution.py
+++ Python/Modules/NumPy/Random/Random data distribution.py
@@ -3,22 +3,22 @@ from numpy import random
 
 List = []
 List_2 = []
-n = int(input("Input number of specific elements you want to add into array: "))
+n = int(input("Input number of specific numbers you want to display: "))
 
-print("Input value of each specific element: ")
+print("Input value of each specific number: ")
 for i in range(n):
-    element = int(input("Element " + str(i + 1)+ " : "))
+    element = int(input("Number " + str(i + 1)+ " : "))
     List.append(element)
 
-print("Input value of probability of each specific element: ")
+print("Input value of probability of each specific number: ")
 for i in range(n):
-    element = float(input("Element " + str(i + 1)+ " : "))
+    element = float(input("Number " + str(i + 1)+ " : "))
     List_2.append(element)
 
 Array = np.array(List)
 P = np.array(List_2)
 
-Size= int(input("Input number of elements you want to display in array: "))
+Size= int(input("Input number of elements you want to display: "))
 display_elements = random.choice(Array, p = P, size=(Size))
 
 print("The random data distribution:", display_elements)
