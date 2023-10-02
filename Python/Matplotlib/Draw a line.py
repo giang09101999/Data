7b9ff9592cd73d3876f9431cac02fd3bf185fa66
giang09101999 <Gnguyen899@gmail.com>
2023-10-02 22:01:49 +0700

    Draw a line

--- Python/Matplotlib/Draw a line.py
+++ Python/Matplotlib/Draw a line.py
@@ -2,9 +2,15 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
-x = np.array([1, 5])
+start_point_x = int(input("Input start point of x: "))
+end_point_x = int(input("Input end point of x: "))
 
-y = np.array([2, 10])
+start_point_y = int(input("Input start point of y: "))
+end_point_y = int(input("Input end point of y: "))
+
+
+x = np.array([start_point_x, end_point_x])
+y = np.array([start_point_y, end_point_y])
 
 plt.plot(x, y)
 plt.show()
