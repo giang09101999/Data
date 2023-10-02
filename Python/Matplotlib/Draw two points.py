6bc9c042928df956aa5c5baa72fd4d99c78a4ef1
giang09101999 <Gnguyen899@gmail.com>
2023-10-02 22:09:59 +0700

    Draw two points

---
+++ Python/Matplotlib/Draw two points.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+
+start_point_x = int(input("Input start point of x: "))
+end_point_x = int(input("Input end point of x: "))
+
+start_point_y = int(input("Input start point of y: "))
+end_point_y = int(input("Input end point of y: "))
+
+x = np.array([start_point_x, end_point_x])
+y = np.array([start_point_y, end_point_y])
+
+plt.plot(x, y, "o")
+plt.show()
