bc280298598a755b04a53532fa127786bdd70f1a
giang09101999 <Gnguyen899@gmail.com>
2023-10-03 22:15:38 +0700

    Compare plots

---
+++ Python/Matplotlib/Compare plots.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x1 = np.array([1, 2, 3])
+y1 = np.array([4, 5, 6])
+plt.scatter(x1, y1)
+
+x2 = np.array([11, 12, 13])
+y2 = np.array([15, 16, 17])
+plt.scatter(x2, y2)
+plt.show()
