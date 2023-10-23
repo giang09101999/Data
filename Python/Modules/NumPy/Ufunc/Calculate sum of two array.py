11bf113db168a25390328ab864ee9696b47a4ef3
giang09101999 <Gnguyen899@gmail.com>
2023-10-23 10:17:56 +0700

    Calculate sum of two arrays

---
+++ Python/Modules/NumPy/Ufunc/Calculate sum of two array.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+X = []
+Y = []
+
+size = int(input("Input size of two array: "))
+
+for i in range(size):
+    element = int(input("X[" + str(i) + "] = "))
+    X.append(element)
+print("======================================")
+for i in range(size):
+    element = int(input("Y[" + str(i) + "] = "))
+    Y.append(element)
+
+print("The sum of two array: ")
+sum = np.add(X, Y)
+print(sum)
